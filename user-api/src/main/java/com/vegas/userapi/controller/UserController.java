d779c82a1e6e213c2771fdfe49686f00d65c6aa5
IMANAKA MASAYUKI <imanaka-m@roboken.iit.tsukuba.ac.jp>
2019-09-03 21:41:16 +0900

    sign_up page complete

--- user-api/src/main/java/com/vegas/userapi/controller/UserController.java
+++ user-api/src/main/java/com/vegas/userapi/controller/UserController.java
@@ -1,12 +1,27 @@
 package com.vegas.userapi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.vegas.userapi.modeldto.User;
+import com.vegas.userapi.service.UserService;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller
 public class UserController {
+ 	private final UserService userService;
     @GetMapping("/sign_up")
-    public String getSignUpPage() {
+    public String getSignUpPage(Model model) {
+    	 model.addAttribute("user",new User());
         return "form";
+    }
+    @PostMapping("/sign_up")
+    public String completeSignUp(@ModelAttribute User user) {
+    	userService.saveUserData(user);
+    	return "index";
     }
 }
