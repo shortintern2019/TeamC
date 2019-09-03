1d12408b3db1f90a2c9276fdc0e36bcffdd2d5f4
IMANAKA MASAYUKI <imanaka-m@roboken.iit.tsukuba.ac.jp>
2019-09-03 15:10:54 +0900

    serviceパッケージを作成

---
+++ user-api/src/main/java/com/vegas/userapi/modeldto/User.java
@@ -0,0 +1,12 @@
+package com.vegas.userapi.modeldto;
+
+import java.io.Serializable;
+
+
+public class User implements Serializable {
+	String name;
+	String emailAddress;
+	Integer age;
+	Integer bodyType;
+	Integer heightType;
+}
---
+++ user-api/src/main/java/com/vegas/userapi/service/UserService.java
@@ -0,0 +1,10 @@
+package com.vegas.userapi.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class UserService {
+	public void saveUserData() {
+	
+}
